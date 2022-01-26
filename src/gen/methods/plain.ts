8f21bbe1f943267ef33064f1f495ba5db3273154
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-01-26 18:44:58 +0100

    Fix an issue with wrong query names been generated
    - added release workflow under @nevermined-io

--- src/gen/methods/plain.ts
+++ src/gen/methods/plain.ts
@@ -1,5 +1,6 @@
+import { convertType } from "gen/util";
 import { heading, multiBody, singleBody, types } from "../segments/body";
-import { Schema } from "../types";
+import { Entity, Field, Schema } from "../types";
 
 /**
  * Writes a single file which contains types and async functions for every queryable entity.
@@ -10,13 +11,22 @@ export default function plain(schema: Schema): string {
 
     out.push(heading());
 
+    const queryEntity = schema.types.filter(e => e.name === 'Query' && e.fields).pop()
+
     for (const entity of schema.types) {
         const filterEntity = schema.types.find(e => e.name === entity.name + '_filter');
         if (!filterEntity) continue;
 
         out.push(types(entity, filterEntity));
-        out.push(`export const get${entity.name}ById = ${singleBody(entity)};`);
-        out.push(`export const get${entity.name}s = ${multiBody(entity)};`);
+
+        const funcName = queryEntity?.fields?.find(f => f.type.name === entity.name)?.name
+        const funcNames =  queryEntity?.fields?.find(f => f.type.ofType?.kind === 'LIST' && f.type.ofType.ofType?.ofType?.name === entity.name)?.name
+        if(!funcName || !funcNames) {
+            throw new TypeError(`Couldn't get query function names for ${entity.name}`)
+        }
+
+        out.push(`export const get${entity.name}ById = ${singleBody(entity, funcName)};`);
+        out.push(`export const get${entity.name}s = ${multiBody(entity, funcNames)};`);
     }
 
     return out.join('\n');
